e92503d19c30c4459cb89d4baf83d79b429a24ab
magnussony <magnuslundstromdk@gmail.com>
2020-04-15 21:28:50 +0200

    lets get em

--- client/src/components/auth/Tunnel.js
+++ client/src/components/auth/Tunnel.js
@@ -45,7 +45,7 @@ class Tunnel extends React.Component {
         </div>
       )
     } else if (this.state.userLoaded) {
-      return <Redirect to="/create-path" />
+      return <Redirect to="/dashboard" />
     } else {
       return <Redirect to="/" />
     }
