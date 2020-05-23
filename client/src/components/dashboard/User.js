194edb6321f4520b0c53a7d2eccf96a6c7fa85ae
magnussony <45564667+magnussony@users.noreply.github.com>
2020-05-23 15:10:35 +0200

    Different paths link & some isAdmin logic

--- client/src/components/dashboard/User.js
+++ client/src/components/dashboard/User.js
@@ -21,9 +21,7 @@ class User extends React.Component {
     const completedPaths = paths.filter((path) => path.isCompleted)
     const unfinishedPaths = paths.filter((path) => !path.isCompleted)
     const { data: user } = await axios.get(`/api/user?id=${id}`)
-    this.setState({ completedPaths, unfinishedPaths, user, loading: false }, () =>
-      console.log(this.state)
-    )
+    this.setState({ completedPaths, unfinishedPaths, user, loading: false }, () => console.log(this.state))
   }
   onScoll = () => {}
 
@@ -32,34 +30,37 @@ class User extends React.Component {
       <Container>
         {(this.state.loading && <BoxLoader />) || (
           <div>
-            <div className="flex justify-between">
-              <div className="flex">
+            <div className='flex justify-between'>
+              <div className='flex'>
                 <img
                   src={`data:image/png;base64, ${this.state.user.image}`}
-                  alt="profile"
-                  className="h-32 w-32 rounded-full mr-6"
+                  alt='profile'
+                  className='h-32 w-32 rounded-full mr-6'
                 />
                 <div>
                   <h1>
                     {this.state.user.firstName} {this.state.user.lastName}
                   </h1>
-                  <p className="font-semibold">{this.state.user.jobTitle}</p>
+                  <p className='font-semibold'>{this.state.user.jobTitle}</p>
                 </div>
               </div>
               {this.props.isAdmin && (
-                <Link to="/create-path" className="">
-                  <i className="fas fa-plus text-2xl font-semibold mt-4 hover-spin"></i>
+                <Link to='/create-path' className=''>
+                  <i className='fas fa-plus text-2xl font-semibold mt-4 hover-spin'></i>
                 </Link>
               )}
             </div>
-            <div className="mt-5">
-              <Link to="/employees" className="font-semibold mb-5 hover-underline">
-                <i className="fas fa-arrow-left mr-1 "></i> Go back to employees
-              </Link>
+            <div className='mt-5'>
+              {this.props.isAdmin && (
+                <Link to='/employees' className='font-semibold mb-5 hover-underline'>
+                  <i className='fas fa-arrow-left mr-1 '></i> Go back to employees
+                </Link>
+              )}
+
               <div>
-                <h2 className="mt-10 -mb-5">Active paths</h2>
+                <h2 className='mt-10 -mb-5'>Active paths</h2>
                 {this.state.unfinishedPaths.length === 0 ? (
-                  <p className="mt-10">User has no active paths</p>
+                  <p className='mt-10'>User has no active paths</p>
                 ) : (
                   <PathList
                     paths={[...this.state.unfinishedPaths]}
@@ -70,9 +71,9 @@ class User extends React.Component {
                 )}
               </div>
               <div>
-                <h2 className="mt-10 -mb-5">Completed paths</h2>
+                <h2 className='mt-10 -mb-5'>Completed paths</h2>
                 {this.state.completedPaths.length === 0 ? (
-                  <p className="mt-10">User has no completed paths</p>
+                  <p className='mt-10'>User has no completed paths</p>
                 ) : (
                   <PathList
                     paths={[...this.state.completedPaths]}
